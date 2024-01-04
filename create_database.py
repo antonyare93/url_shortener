11223b2447780fc58902402e1dbaa4e4fba5bdfd
Antony Arévalo <antonyare_93@hotmail.com>
2024-01-03 20:35:43 -0500

    Initialize_database

--- create_database.py
+++ create_database.py
@@ -5,6 +5,13 @@ connection = sqlite3.connect('urls_database.db')
 
 cursor = connection.cursor()
 
+cursor.execute("""
+CREATE TABLE urls (
+  url TEXT,
+  id INTEGER PRIMARY KEY,
+  count INTEGER
+)
+""")
 cursor.execute("INSERT INTO urls (url, id, count) VALUES ('https://wa.me/message/SG6D2NUNM5VIJ1', 'wa_delbuey_pag', 0)")
 
 connection.commit()
